ea41afd64034b631e5557198b6ee524d1503a334
aacgmail <96009550+aacgmail@users.noreply.github.com>
2022-10-12 16:09:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,16 +43,7 @@ try:
 except URLError as e:
   streamlit.error()
 
-#import requests
-#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
-
-# take the json version of the response and normalize it
-#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# output it the screen as a table
-#streamlit.dataframe(fruityvice_normalized)
-# don't run anything past here we troubleshoot
-#streamlit.stop()
 
 import snowflake.connector
 
@@ -67,16 +58,18 @@ def get_fruit_load_list():
 if streamlit.button('Get Fruit Load List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
+    my_cnx.close()
     streamlit.dataframe(my_data_rows)
   
 # Allow the end user to add a fruit to the list
 def insert_row_snowflake(new_fruit):
   with my_cnx.cursor() as my_cur:
-     my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+     my_cur.execute("insert into fruit_load_list values ('" + fruit_load_list +"')")
      return "Thanks for adding " + new_fruit
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a Fruit to the List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   back_from_function = insert_row_snowflake(add_my_fruit)
+  my_cnx.close()
   streamlit.text(back_from_function)
